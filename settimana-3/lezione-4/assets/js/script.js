f86975f91a42a84feef88893139c42e74e5ff1f2
Martina Cretella <martina.cretella@outlook.it>
2024-02-08 18:11:53 +0100

    settimana 3 lezione 4 (tombola)

--- settimana-3/lezione-4/assets/js/script.js
+++ settimana-3/lezione-4/assets/js/script.js
@@ -1,29 +1,55 @@
-// 1. creare una griglia di 90 numeri, su 9 righe ognuna delle quali contiene i div necessari (da 1 a 10, da 11 a 20, da 21 a 30...)
-const grigliaNumeri = (numeriNellaGriglia) => {
-    const grigliaDiv = document.getElementById('griglia');
-    for (let i = 0; i < 10; i++) {
-        const celleNumeri1 = document.createElement('div');
-        celleNumeri1.classList.add('numeri1'); //da 1 a 10
+const tombolone = document.getElementById('tombolone');
+const btnEstrai = document.getElementById('estrai');
+const btnReset = document.getElementById('reset');
+const estratti = [];
+
+window.addEventListener('load', init);
+
+function init() {
+    creaTabellone();
+}
+
+const creaTabellone = () => {
+    for (i = 1; i < 91; i++) {
+        const casella = document.createElement('div');
+        const testo = document.createElement('span');
+        testo.innerText = i;
+        testo.classList.add('numeri');
+        casella.classList.add('caselle');
+        casella.appendChild(testo);
+        tombolone.appendChild(casella);
     }
-    for (let i = 10; i < 20)
 }
 
+btnEstrai.addEventListener('click', function() {
+    estrai();
+});
 
+btnReset.addEventListener('click', function() {
+    const tabellone = document.querySelectorAll('.caselle');
+    tabellone.forEach((casella) => {
+        casella.classList.remove('estratto');
+    });
+    estratti.splice(0);
+    document.getElementById('estratto').innerText = '';
+});
 
-// 2. creare un button che generi un numero random tra 1 e 90 (e ovviamente stampare il numero estratto)
-function pescaNumero() {
-    const 
+function estrai() {
+    let estratto = Math.floor((Math.random() * 90) + 1);
+    if (estratti.includes(estratto)) {
+        estrai();
+    } else {
+        document.getElementById('estratto').innerText = `Numero estratto: ${estratto}`;
+        estratti.push(estratto);
+        colora(estratto);
+    }
 }
 
-// 3. evidenziare sul tombolone il numero estratto
-
-// 4. verificare che il numero estratto non sia già uscito: se è uscito, estrarre un nuovo numero (funzione ricorsiva: è una funzione che
-// che a condizione chiama se stessa)
-
-/* function estraiNumero () {
-        numero = Math.floor(Math.random() eccetera)
-        if (numero estratto è presente nell'array estratti) {
-                estraiNumero();
+function colora(estratto) {
+    const tabellone = document.querySelectorAll('.caselle');
+    for (let i = 0; i < tabellone.length; i++) {
+        if (parseInt(tabellone[i].innerText) === estratto) {
+            tabellone[i].classList.add('estratto');
         }
+    }
 }
-*/
