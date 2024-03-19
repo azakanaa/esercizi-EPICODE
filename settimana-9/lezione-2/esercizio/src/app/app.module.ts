1d03500f84aa5fb39983da4700206cdb12424b47
Martina Cretella <martina.cretella@outlook.it>
2024-03-19 13:02:30 +0100

    angular

--- settimana-9/lezione-2/esercizio/src/app/app.module.ts
+++ settimana-9/lezione-2/esercizio/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './components/component1/component1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component
   ],
   imports: [
     BrowserModule
