aae60aa4d0f97c78067bb35b77a4232b9c6f7664
Martina Cretella <martina.cretella@outlook.it>
2024-03-19 10:49:26 +0100

    settimana 9 lezione 1

---
+++ settimana-9/lezione-1/assets/ts/script.ts
@@ -0,0 +1,13 @@
+interface Chiamate {
+    credito: number;
+    numeroChiamate: number;
+}
+
+class User {
+    nome: string;
+    cognome: string;
+    constructor(_nome: string, _cognome: string) {
+        this.nome = _nome;
+        this.cognome = _cognome;
+    }
+}
